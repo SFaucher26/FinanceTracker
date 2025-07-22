392d24a1a3ddc8e63c6a1cb7c1505bebf21c9e67
SFaucher26 <sandrine.faucher@maif.fr>
2025-07-22 15:40:19 +0200

    projet de depart

---
+++ src/main/java/com/simplon/FinanceTracker/FinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.FinanceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApplication.class, args);
+	}
+
+}
